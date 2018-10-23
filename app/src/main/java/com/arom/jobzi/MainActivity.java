29666657b770706580ba8c5420cf1a75f4dad24b
Mohammed Alshehri <moe.ghanim.alshehri@gmail.com>
2018-10-23 17:00:46 -0400

    update login information

--- app/src/main/java/com/arom/jobzi/MainActivity.java
+++ app/src/main/java/com/arom/jobzi/MainActivity.java
@@ -2,14 +2,48 @@ package com.arom.jobzi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView usernameTextView;
+    private TextView passwordTextView;
+
+    private Button loginButton;
+    private Button signupButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usernameTextView = findViewById(R.id.userName);
+        passwordTextView = findViewById(R.id.userPassword);
+
+        signupButton = findViewById(R.id.button_signUp);
+
+        loginButton = findViewById(R.id.button_logIn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = usernameTextView.getText().toString();
+                String password = passwordTextView.getText().toString();
+
+
+
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
