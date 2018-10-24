f20ab90a80b8d766efa8eb0de0581b9952bd3ee5
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-10-24 14:33:14 -0400

    Working on adding signup functionality.
    Trying to figure out proper way to add and authenticate users using Firebase Authentication and Firebase Realtime Database.

--- app/src/main/java/com/arom/jobzi/LoginActivity.java
+++ app/src/main/java/com/arom/jobzi/LoginActivity.java
@@ -7,7 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.arom.jobzi.account.AccountsManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,20 +17,22 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
     private Button signupButton;
-    
-    private AccountsManager database;
+
+    private FirebaseAuth authentication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        authentication = FirebaseAuth.getInstance();
+
         usernameTextView = findViewById(R.id.userName);
         passwordTextView = findViewById(R.id.userPassword);
 
-        signupButton = findViewById(R.id.button_signUp);
+        signupButton = findViewById(R.id.gotoSignupButton);
 
-        loginButton = findViewById(R.id.button_logIn);
+        loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +53,19 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = authentication.getCurrentUser();
+
+        if(currentUser != null) {
+            Intent toWelcomeIntent = new Intent(this, WelcomeActivity.class);
+            startActivity(toWelcomeIntent);
+        }
 
     }
 }
--- app/src/main/java/com/arom/jobzi/account/AccountType.java
+++ app/src/main/java/com/arom/jobzi/account/AccountType.java
@@ -2,6 +2,16 @@ package com.arom.jobzi.account;
 
 public enum AccountType {
 
-    ADMIN, HOME_OWNER, SERVICE_PROVIDER;
+    ADMIN("Admin"), HOME_OWNER("A Home Owner"), SERVICE_PROVIDER("A Service Provider");
 
+    private final String name;
+
+    private AccountType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
