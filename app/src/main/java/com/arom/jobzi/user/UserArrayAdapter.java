cc33450b542ddce87f25e0581f86a56cbcd7d738
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-06 18:25:01 -0500

    Adding service adding/editing functionality.
    - Added Service class to hold information.
    - Still need to add a list of all the services so the admin can see them and edit them.

--- app/src/main/java/com/arom/jobzi/user/UserArrayAdapter.java
+++ app/src/main/java/com/arom/jobzi/user/UserArrayAdapter.java
@@ -12,12 +12,12 @@ import com.arom.jobzi.R;
 
 import java.util.List;
 
-public class UserList extends ArrayAdapter<User> {
+public class UserArrayAdapter extends ArrayAdapter<User> {
 
     private Activity context;
     private List<User> users;
 
-    public UserList(Activity context, List<User> users) {
+    public UserArrayAdapter(Activity context, List<User> users) {
         super(context, R.layout.user_item);
 
         this.context = context;
