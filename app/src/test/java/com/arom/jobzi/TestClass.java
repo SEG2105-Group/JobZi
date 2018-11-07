860fbe7b93463ecc2d11bf696cf8805c002ecbbb
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-06 22:59:32 -0500

    Removed non-existant import.

--- app/src/test/java/com/arom/jobzi/TestClass.java
+++ app/src/test/java/com/arom/jobzi/TestClass.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import com.vogella.android.temperature.ConverterUtil;
 
 public class TestClass {
     @Test
