0ad6088783c21b9d3d0bfd2a786f70d5eaf2520c
OmarAl <omar.almoallim@gmail.com>
2018-11-08 00:13:56 -0500

    services fragment work (still needs some more work

--- app/src/main/java/com/arom/jobzi/service/ServicesFragment.java
+++ app/src/main/java/com/arom/jobzi/service/ServicesFragment.java
@@ -7,24 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.arom.jobzi.R;
+import com.arom.jobzi.user.User;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ServicesFragment extends Fragment {
-
-    View view;
-    //private List<Services> services;
+    private View view;
     private DatabaseReference accountsDatabase;
-
-    public ServicesFragment(){
-
-    }
+    private Service service;
+    private List<Service> services;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.service_item, container, false);
+        view = inflater.inflate(R.layout.user_item, container, false);
+        services = new LinkedList<Service>();
+
+        ListView listView = view.findViewById(R.id.fragmentuServiceList);
+
+        ServiceArrayAdapter listViewServiceAdapter = new ServiceArrayAdapter(getActivity(), services);
+        listView.setAdapter(listViewServiceAdapter);
 
         return view;
     }
