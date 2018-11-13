4cb981a49759408200fd3477d7b3410627302d95
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-13 16:21:55 -0500

    Trying to improve navigation header.
    - Added ProfileActivity, not implemented.
    - Added AvaialbleTimeSlot, not sure how to make this work.

---
+++ app/src/main/java/com/arom/jobzi/service/AvailableTimeSlot.java
@@ -0,0 +1,17 @@
+package com.arom.jobzi.service;
+
+import java.util.Date;
+
+public class AvailableTimeSlot {
+
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
