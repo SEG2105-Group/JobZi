dc28433b378e3aaf0f1129bc3dc6cbceeb1fe611
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-15 23:03:22 -0500

    Cleaned up signup activity, which had a firebase auth reference.

--- app/src/main/java/com/arom/jobzi/SignupActivity.java
+++ app/src/main/java/com/arom/jobzi/SignupActivity.java
@@ -21,7 +21,6 @@ import com.arom.jobzi.util.Util;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -46,16 +45,12 @@ public class SignupActivity extends AppCompatActivity {
     private Button signupButton;
     private Button backButton;
 
-    private FirebaseAuth auth;
-
     private boolean adminExists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
-        auth = FirebaseAuth.getInstance();
 
         Util.getInstance().addSingleValueAccountsListener(new ValueEventListener() {
             @Override
