e23b90d787acb23221a3b930218d24ca5a811f2e
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-15 22:42:05 -0500

    Working on making fragment code more reusable.
    - ServiceListFragment, as it is right now, cannot be re-used between service provider and admin. Should probably just cut this out entirely.

--- app/src/main/java/com/arom/jobzi/fragment/AdminFragment.java
+++ app/src/main/java/com/arom/jobzi/fragment/AdminFragment.java
@@ -1,5 +1,6 @@
 package com.arom.jobzi.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,8 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.arom.jobzi.R;
+import com.arom.jobzi.ServiceEditorActivity;
+import com.arom.jobzi.service.Service;
+import com.arom.jobzi.util.Util;
 
-public class AdminFragment extends Fragment {
+public class AdminFragment extends Fragment implements ServiceListFragment.ServiceItemListener, DeleteServiceDialogFragment.DeleteServiceListener {
 	
 	@Nullable
 	@Override
@@ -34,4 +38,51 @@ public class AdminFragment extends Fragment {
 		return view;
 		
 	}
+
+	@Override
+	public void onDelete(Service service) {
+        Util.getInstance().deleteService(service);
+	}
+
+	@Override
+	public void onClick(Service service) {
+
+        Intent toServiceEditorIntent = new Intent(this.getActivity(), ServiceEditorActivity.class);
+        toServiceEditorIntent.putExtra(ServiceEditorActivity.SERVICE_BUNDLE_ARG, service);
+        toServiceEditorIntent.putExtra(ServiceEditorActivity.NEW_SERVICE_MODE_BUNDLE_ARG, false);
+
+        startActivity(toServiceEditorIntent);
+
+	}
+
+	@Override
+	public void onLongClick(Service service) {
+
+        DeleteServiceDialogFragment deleteServiceDialogFragment = new DeleteServiceDialogFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(DeleteServiceDialogFragment.LISTENER_BUNDLE_ARG, this);
+        bundle.putSerializable(DeleteServiceDialogFragment.SERVICE_BUNDLE_ARG, service);
+
+        deleteServiceDialogFragment.setArguments(bundle);
+
+        deleteServiceDialogFragment.show(this.getActivity().getSupportFragmentManager(), "");
+
+	}
+
+    @Override
+    public void addService() {
+
+        Service service = new Service();
+
+        service.setName("");
+        service.setRate(0);
+
+        Intent toServiceEditorIntent = new Intent(getActivity(), ServiceEditorActivity.class);
+        toServiceEditorIntent.putExtra(ServiceEditorActivity.SERVICE_BUNDLE_ARG, service);
+        toServiceEditorIntent.putExtra(ServiceEditorActivity.NEW_SERVICE_MODE_BUNDLE_ARG, true);
+        startActivity(toServiceEditorIntent);
+
+    }
+
 }
--- app/src/main/java/com/arom/jobzi/fragment/ServiceListFragment.java
+++ app/src/main/java/com/arom/jobzi/fragment/ServiceListFragment.java
@@ -1,10 +1,12 @@
 package com.arom.jobzi.fragment;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,122 +14,142 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.arom.jobzi.R;
-import com.arom.jobzi.ServiceEditorActivity;
 import com.arom.jobzi.service.Service;
 import com.arom.jobzi.service.ServiceArrayAdapter;
 import com.arom.jobzi.util.Util;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceListFragment extends Fragment {
-	
-	private List<Service> serviceList;
-	
-	private FloatingActionButton addServiceFloatingButton;
 
-	private ServiceItemListener listener;
-	
-	public ServiceListFragment() {
-	
-	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		serviceList = new ArrayList<Service>();
-		
-	}
+    public static final String LISTENER_BUNDLE_ARG = "listener";
 
-	@Override
-	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		
-		View view = inflater.inflate(R.layout.fragment_service_list, container, false);
-		
-		ListView serviceListView = view.findViewById(R.id.serviceListView);
-		
-		ServiceArrayAdapter serviceArrayAdapter = new ServiceArrayAdapter(getActivity(), serviceList);
-		
-		serviceListView.setAdapter(serviceArrayAdapter);
-		
-		serviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				
-				Service service = serviceList.get(position);
-				
-				Intent toServiceEditorIntent = new Intent(ServiceListFragment.this.getActivity(), ServiceEditorActivity.class);
-				toServiceEditorIntent.putExtra(ServiceEditorActivity.SERVICE_BUNDLE_ARG, service);
-				toServiceEditorIntent.putExtra(ServiceEditorActivity.NEW_SERVICE_MODE_BUNDLE_ARG, false);
-				
-				ServiceListFragment.this.startActivity(toServiceEditorIntent);
-				
-			}
-		});
-		
-		serviceListView.setLongClickable(true);
-		serviceListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-				
-				DeleteServiceDialogFragment deleteServiceDialogFragment = new DeleteServiceDialogFragment();
-				
-				Bundle bundle = new Bundle();
-				bundle.putSerializable(DeleteServiceDialogFragment.SERVICE_BUNDLE_ARG, serviceList.get(i));
-				
-				deleteServiceDialogFragment.setArguments(bundle);
-				
-				deleteServiceDialogFragment.show(ServiceListFragment.this.getActivity().getSupportFragmentManager(), "");
-				
-				return true;
-				
-			}
-		});
-		
-		Util.getInstance().addServiceListListener(serviceArrayAdapter, serviceList);
-		
-		addServiceFloatingButton = view.findViewById(R.id.addServiceFloatingButton);
-		
-		addServiceFloatingButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				ServiceListFragment.this.addService();
-			}
-		});
-		
-		return view;
-		
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		
-		addServiceFloatingButton.setEnabled(true);
-		
-	}
-	
-	private void addService() {
-		
-		addServiceFloatingButton.setEnabled(false);
-		
-		Service service = new Service();
-		
-		service.setName("");
-		service.setRate(0);
-		
-		Intent toServiceEditorIntent = new Intent(ServiceListFragment.this.getActivity(), ServiceEditorActivity.class);
-		toServiceEditorIntent.putExtra(ServiceEditorActivity.SERVICE_BUNDLE_ARG, service);
-		toServiceEditorIntent.putExtra(ServiceEditorActivity.NEW_SERVICE_MODE_BUNDLE_ARG, true);
-		ServiceListFragment.this.startActivity(toServiceEditorIntent);
-		
-	}
+    private List<Service> serviceList;
 
-	public interface ServiceItemListener {
+    private FloatingActionButton addServiceFloatingButton;
 
-	    void onClick(Service service);
+    private ServiceItemListener listener;
+
+    public ServiceListFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        serviceList = new ArrayList<Service>();
+
+        Bundle arguments = getArguments();
+
+        if (arguments != null) {
+            listener = (ServiceItemListener) arguments.get(LISTENER_BUNDLE_ARG);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_service_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView serviceListView = view.findViewById(R.id.serviceListView);
+
+        ServiceArrayAdapter serviceArrayAdapter = new ServiceArrayAdapter(getActivity(), serviceList);
+
+        serviceListView.setAdapter(serviceArrayAdapter);
+
+        serviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (listener != null) {
+                    listener.onClick(serviceList.get(position));
+                }
+
+            }
+        });
+
+        serviceListView.setLongClickable(true);
+        serviceListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (listener != null) {
+                    listener.onLongClick(serviceList.get(i));
+                }
+
+                return true;
+
+            }
+        });
+
+        Util.getInstance().addServiceListListener(serviceArrayAdapter, serviceList);
+
+        addServiceFloatingButton = view.findViewById(R.id.addServiceFloatingButton);
+
+        addServiceFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addServiceFloatingButton.setEnabled(false);
+
+                if(listener != null) {
+                    listener.addService();
+                }
+
+                ServiceListFragment.this.addService();
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        addServiceFloatingButton.setEnabled(true);
+
+    }
+
+    private void addService() {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof ServiceItemListener) {
+            listener = (ServiceItemListener) context;
+        } else {
+            Log.w(ServiceListFragment.class.getName(), "The context attaching this service list fragment does not implement the ServiceItemListener interface.");
+        }
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        listener = null;
+
+    }
+
+    public interface ServiceItemListener extends Serializable {
+
+        void onClick(Service service);
+
+        void onLongClick(Service service);
+
+        void addService();
 
     }
 
