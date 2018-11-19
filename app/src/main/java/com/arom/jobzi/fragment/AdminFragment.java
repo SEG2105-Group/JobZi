55373da17fb9847bd1d9a8979cd5d0fe284542b4
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-19 18:37:21 -0500

    Admin updates all service provider profiles when a service is deleted.

--- app/src/main/java/com/arom/jobzi/fragment/AdminFragment.java
+++ app/src/main/java/com/arom/jobzi/fragment/AdminFragment.java
@@ -13,11 +13,16 @@ import android.view.ViewGroup;
 import com.arom.jobzi.R;
 import com.arom.jobzi.ServiceEditorActivity;
 import com.arom.jobzi.fragment.admin.AdminServicesFragment;
+import com.arom.jobzi.profile.ServiceProviderProfile;
 import com.arom.jobzi.service.Service;
 import com.arom.jobzi.util.Util;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class AdminFragment extends Fragment implements AdminServicesFragment.ServiceItemListener, DeleteServiceDialogFragment.DeleteServiceListener {
 	
@@ -48,10 +53,41 @@ public class AdminFragment extends Fragment implements AdminServicesFragment.Ser
 	}
 	
 	@Override
-	public void onDelete(Service service) {
+	public void onDelete(final Service service) {
   
 		DatabaseReference servicesDatabase = Util.getInstance().getServicesDatabase();
 		servicesDatabase.child(service.getId()).removeValue();
+		
+		final DatabaseReference profilesDatabase = Util.getInstance().getProfilesDatabase();
+		profilesDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                
+                for(DataSnapshot profileSnapshot: dataSnapshot.getChildren()) {
+    
+                    try {
+                        
+                        ServiceProviderProfile serviceProviderProfile = profileSnapshot.getValue(ServiceProviderProfile.class);
+    
+                        List<Service> services = serviceProviderProfile.getServices();
+                        
+                        services.remove(service);
+                        
+                        profilesDatabase.child(profileSnapshot.getKey()).setValue(serviceProviderProfile);
+                        
+                    } catch(Exception ex) {
+                        // Do nothing.
+                    }
+                    
+                }
+                
+            }
+            
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            
+            }
+        });
 		
 	}
 
