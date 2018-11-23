3fab38eb0f6899f45de3d57d84e041a40ae8bfdd
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-23 18:29:13 -0500

    Time picker now actually uses the time it is passed.
    - The initial time shown is the one it is passed in from the intent's arguments giving the feeling of editing the time.

--- app/src/main/java/com/arom/jobzi/AvailableTimeSlotEditorActivity.java
+++ app/src/main/java/com/arom/jobzi/AvailableTimeSlotEditorActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,102 +18,101 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class AvailableTimeSlotEditorActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
-    
+
     public static final String DAY_TO_EDIT_BUNDLE_ARG = "day";
     public static final String AVAILABILITY_BUNDLE_ARG = "availability";
     public static final String AVAILABILITY_INDEX_BUNDLE_ARG = "availability_index";
-    
+
     public static final int AVAILABILITY_ADDED_RESULT = 0;
     public static final int AVAILABILITY_SAVED_RESULT = 1;
     public static final int CANCEL_RESULT = 2;
-    
-    private TextView selectStartTime, selectEndTime;
-    private TextView startTimeViewer, endTimeViewer;
-    private Button saveButton, cancelButton;
+
+    private TextView startTimeTextView;
+    private TextView endTimeTextView;
+    private Button saveButton;
+    private Button cancelButton;
+
     private Calendar calendar;
     private Availability availability;
-    
+
     private String day;
     private int availabilityIndex;
-    
+
     private String amPm;
-    
+
     private int hour, minute;
     private boolean startFlag, endFlag;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_time_slot_editor);
-        
+
         calendar = Calendar.getInstance();
         hour = calendar.get(Calendar.HOUR_OF_DAY);
         minute = calendar.get(Calendar.MINUTE);
-        
-        startTimeViewer = findViewById(R.id.startTimeTextView);
-        endTimeViewer = findViewById(R.id.endTimeTextView);
-        
-        Bundle bundle = getIntent().getExtras();
-        
-        day = bundle.getString(DAY_TO_EDIT_BUNDLE_ARG);
-        availabilityIndex = bundle.getInt(AVAILABILITY_INDEX_BUNDLE_ARG);
-        availability = (Availability) bundle.getSerializable(AVAILABILITY_BUNDLE_ARG);
-        
-        if (availability == null) {
-            
-            availability = new Availability();
-            
-            availability.setStartTime(calendar.getTime());
-            
-            // Increment by 30 minutes.
-            calendar.add(Calendar.MINUTE, 30);
-            
-            availability.setEndTime(calendar.getTime());
-            
-        }
-        
-        updateTimeViews();
-        
-        selectStartTime = findViewById(R.id.startTimeTextView);
-        selectStartTime.setOnClickListener(new View.OnClickListener() {
+
+        startTimeTextView = findViewById(R.id.startTimeTextView);
+        startTimeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showTimePickerDialog(availability.getStartTime());
                 startFlag = true;
             }
         });
-        
-        selectEndTime = findViewById(R.id.endTimeTextView);
-        selectEndTime.setOnClickListener(new View.OnClickListener() {
+
+        endTimeTextView = findViewById(R.id.endTimeTextView);
+        endTimeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showTimePickerDialog(availability.getEndTime());
                 endFlag = true;
             }
         });
-        
+
+        Bundle bundle = getIntent().getExtras();
+
+        day = bundle.getString(DAY_TO_EDIT_BUNDLE_ARG);
+        availabilityIndex = bundle.getInt(AVAILABILITY_INDEX_BUNDLE_ARG);
+        availability = (Availability) bundle.getSerializable(AVAILABILITY_BUNDLE_ARG);
+
+        if (availability == null) {
+
+            availability = new Availability();
+
+            availability.setStartTime(calendar.getTime());
+
+            // Increment by 30 minutes.
+            calendar.add(Calendar.MINUTE, 30);
+
+            availability.setEndTime(calendar.getTime());
+
+        }
+
+        updateTimeViews();
+
         saveButton = findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
                 saveButton.setEnabled(false);
-                
+
                 Intent result = new Intent();
                 Bundle bundle = new Bundle();
-                
+
                 bundle.putString(DAY_TO_EDIT_BUNDLE_ARG, day);
                 bundle.putInt(AVAILABILITY_INDEX_BUNDLE_ARG, availabilityIndex);
                 bundle.putSerializable(AVAILABILITY_BUNDLE_ARG, availability);
-    
+
                 result.putExtras(bundle);
-                
+
                 AvailableTimeSlotEditorActivity.this.setResult(AVAILABILITY_SAVED_RESULT, result);
                 AvailableTimeSlotEditorActivity.this.finish();
-                
+
             }
         });
-        
+
         cancelButton = findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,66 +121,53 @@ public class AvailableTimeSlotEditorActivity extends AppCompatActivity implement
                 AvailableTimeSlotEditorActivity.this.finish();
             }
         });
-        
-        endTimeViewer = findViewById(R.id.endTimeTextView);
+
     }
-    
+
     private void updateTimeViews() {
-        
-        startTimeViewer.setText(TimeFormatterUtil.formatTime(this, availability.getStartTime()));
-        endTimeViewer.setText(TimeFormatterUtil.formatTime(this, availability.getEndTime()));
-        
+
+        startTimeTextView.setText(TimeFormatterUtil.formatTime(this, availability.getStartTime()));
+        endTimeTextView.setText(TimeFormatterUtil.formatTime(this, availability.getEndTime()));
+
     }
-    
+
     @Override
     public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-        
+
         if (startFlag) {
-            
+
             calendar.set(Calendar.HOUR, hour);
             calendar.set(Calendar.MINUTE, minute);
-            
+
             availability.getStartTime().setTime(calendar.getTimeInMillis());
-            
+
             startFlag = false;
-            
+
         } else if (endFlag) {
-            
+
             calendar.set(Calendar.HOUR, hour);
             calendar.set(Calendar.MINUTE, minute);
-            
+
             availability.getEndTime().setTime(calendar.getTimeInMillis());
-            
+
             endFlag = false;
         }
-        
+
         updateTimeViews();
-        
+
     }
-    
-    public void showTimePickerDialog(Date timeSlot) {
-        
+
+    public void showTimePickerDialog(Date time) {
+
         DialogFragment timePickerFrag = new TimePickerFragment();
-        
+
         Bundle bundle = new Bundle();
-        bundle.putSerializable(TimePickerFragment.TIME_SLOT_BUNDLE_ARG, timeSlot);
-        
+        bundle.putSerializable(TimePickerFragment.TIME_TO_EDIT_BUNDLE_ARG, time);
+
         timePickerFrag.setArguments(bundle);
-        
+
         timePickerFrag.show(getSupportFragmentManager(), "");
-        
+
     }
-    
-    public String getAMPM() {
-        amPm = "";
-        if (!DateFormat.is24HourFormat(this)) {
-            if (hour >= 12) {
-                amPm = "PM";
-            } else {
-                amPm = "AM";
-            }
-        }
-        return amPm;
-    }
-    
+
 }
--- app/src/main/java/com/arom/jobzi/fragment/TimePickerFragment.java
+++ app/src/main/java/com/arom/jobzi/fragment/TimePickerFragment.java
@@ -13,16 +13,22 @@ import java.util.Date;
 
 public class TimePickerFragment extends DialogFragment {
     
-    public static final String TIME_SLOT_BUNDLE_ARG = "timeslot";
+    public static final String TIME_TO_EDIT_BUNDLE_ARG = "timeslot";
 
     @NonNull
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
+        Bundle bundle = getArguments();
+
+        Date timeToEdit = (Date) bundle.getSerializable(TIME_TO_EDIT_BUNDLE_ARG);
+
         Calendar calender = Calendar.getInstance();
+        calender.setTime(timeToEdit);
+
         int hour = calender.get(Calendar.HOUR_OF_DAY);
         int minute = calender.get(Calendar.MINUTE);
         
-        Date timeSlot = (Date) getArguments().getSerializable(TIME_SLOT_BUNDLE_ARG);
+        Date timeSlot = (Date) getArguments().getSerializable(TIME_TO_EDIT_BUNDLE_ARG);
         calender.setTime(timeSlot);
         
         return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
