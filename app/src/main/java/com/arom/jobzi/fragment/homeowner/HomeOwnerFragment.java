afc5ba5cb1598c019bde1b7be9fd0dd70431ae80
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-27 23:50:12 -0500

    Added most UI elements in the HomeOwnerSearchActivity.
    - Added and properly laid out the UI elements needed to search for a service provider.
    - Saving does not work; only going back from this activity.
    - Made progress on issue #146.

--- app/src/main/java/com/arom/jobzi/fragment/homeowner/HomeOwnerFragment.java
+++ app/src/main/java/com/arom/jobzi/fragment/homeowner/HomeOwnerFragment.java
@@ -1,11 +1,16 @@
 package com.arom.jobzi.fragment.homeowner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.arom.jobzi.HomeOwnerSearchActivity;
 import com.arom.jobzi.R;
 
 public class HomeOwnerFragment extends Fragment {
@@ -15,5 +20,20 @@ public class HomeOwnerFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_home_owner, container, false);
 	}
-
+	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		
+		FloatingActionButton searchForProviderActionButton = view.findViewById(R.id.searchForServiceProviderButton);
+		
+		searchForProviderActionButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent toHomeOwnerSearchIntent = new Intent(HomeOwnerFragment.this.getContext(), HomeOwnerSearchActivity.class);
+				HomeOwnerFragment.this.getActivity().startActivity(toHomeOwnerSearchIntent);
+			}
+		});
+		
+	}
 }
