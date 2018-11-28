bbce0f2c5e7865183b5618bea0105585946568cf
Rawad Aboudlal <rawadaboudlal@yahoo.com>
2018-11-27 22:32:16 -0500

    Added proper activity for searching for service providers.

---
+++ app/src/main/java/com/arom/jobzi/HomeOwnerSearchActivity.java
@@ -0,0 +1,13 @@
+package com.arom.jobzi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeOwnerSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_owner_search);
+    }
+}
