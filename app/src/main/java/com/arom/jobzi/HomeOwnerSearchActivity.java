045a6e96ab4a0de2ae48f85f56fa0bd15b7ae2b0
OmarAl <omar.almoallim@gmail.com>
2018-12-01 15:04:20 -0500

    user can now view the list of services available.

--- app/src/main/java/com/arom/jobzi/HomeOwnerSearchActivity.java
+++ app/src/main/java/com/arom/jobzi/HomeOwnerSearchActivity.java
@@ -1,13 +1,25 @@
 package com.arom.jobzi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.arom.jobzi.adapater.ServiceArrayAdapter;
+import com.arom.jobzi.fragment.serviceprovider.ServiceProviderServicesFragment;
+import com.arom.jobzi.service.Service;
+
+import java.util.List;
 
 public class HomeOwnerSearchActivity extends AppCompatActivity {
+
+    private ServiceArrayAdapter serviceArrayAdapter;
+    private List<Service> services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,16 @@ public class HomeOwnerSearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 HomeOwnerSearchActivity.this.finish();
+            }
+        });
+
+
+        Button setService = findViewById(R.id.setServiceButton);
+        setService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeOwnerSearchActivity.this, ServiceSelectorActivity.class);
+                startActivity(intent);
             }
         });
         
